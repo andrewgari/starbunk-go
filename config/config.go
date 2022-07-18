ebf091fabe1e28db4cfa3e577292a9686bc6f18b
Andrew <covadax.ag@gmail.com>
2022-07-18 19:33:46 -0400

    Setup discord bot

---
+++ config/config.go
@@ -0,0 +1,41 @@
+package config
+
+//it will be used to help us read our config.json file.
+import (
+	"encoding/json"
+	"fmt"       //used to print errors majorly.
+	"io/ioutil" //it will be used to help us read our config.json file.
+)
+
+var (
+	Token     string //To store value of Token from config.json .
+	BotPrefix string // To store value of BotPrefix from config.json.
+
+	config *configStruct //To store value extracted from config.json.
+)
+
+type configStruct struct {
+	Token     string `json : "Token"`
+	BotPrefix string `json : "BotPrefix"`
+}
+
+func ReadConfig() error {
+	fmt.Println("Reading Config File")
+	file, err := ioutil.ReadFile("./config.json")
+	if err != nil {
+		fmt.Println(err.Error())
+		return err
+	}
+
+	fmt.Println(string(file))
+	err = json.Unmarshal(file, &config)
+	if err != nil {
+		fmt.Println(err.Error())
+		return err
+	}
+
+	Token = config.Token
+	BotPrefix = config.BotPrefix
+
+	return nil
+}
