fe8ba781b4230426915550bdc8e0f45c3a1f3154
Andrew <covadax.ag@gmail.com>
2022-08-09 00:20:26 -0400

    Implemented picklebot

---
+++ internal/replybot/PickleBot.go
@@ -0,0 +1,38 @@
+package replybot
+
+import (
+	"golang-discord-bot/internal/config"
+	"golang-discord-bot/internal/webhook"
+	"golang-discord-bot/utils"
+
+	"github.com/bwmarrin/discordgo"
+)
+
+type PickleBot struct {
+}
+
+func (b PickleBot) id() string {
+	return config.UserIDs["Sig"]
+}
+
+func (b PickleBot) ObserverName() string {
+	return "GremlinBot"
+}
+
+func (b PickleBot) AvatarURL() string {
+	return "https://i.imgur.com/D0czJFu.jpg"
+}
+
+func (b PickleBot) Pattern() string {
+	return "gremlin"
+}
+
+func (b PickleBot) Response() string {
+	return "Could you repeat that? I don't speak gremlin..."
+}
+
+func (b PickleBot) HandleMessage(session *discordgo.Session, message *discordgo.Message) {
+	if utils.Match(b.Pattern(), message.Content) || (message.Author.ID == b.id() && roll20(15)) {
+		webhook.WriteMessage(session, message.ChannelID, b.Response(), b.ObserverName(), b.AvatarURL())
+	}
+}
--- internal/replybot/VennBot.go
+++ internal/replybot/VennBot.go
@@ -3,8 +3,6 @@ package replybot
 import (
 	"golang-discord-bot/internal/config"
 	"golang-discord-bot/internal/webhook"
-	"math/rand"
-	"time"
 
 	"github.com/bwmarrin/discordgo"
 )
@@ -12,9 +10,9 @@ import (
 type VennBot struct {
 }
 
-var id string = config.UserIDs["Venn"]
-
-const response string = "Sorry, but that was über cringe..."
+func (b VennBot) id() string {
+	return config.UserIDs["Venn"]
+}
 
 func (b VennBot) ObserverName() string {
 	return ""
@@ -24,14 +22,14 @@ func (b VennBot) AvatarURL() string {
 	return ""
 }
 
+func (b VennBot) Response() string {
+	return "Sorry, but that was über cringe..."
+}
+
 func (b VennBot) HandleMessage(session *discordgo.Session, message *discordgo.Message) {
-	if message.Author.ID == id {
-		rand.Seed(time.Now().UnixNano())
-		var r = rand.Intn(20-1) + 1
-		if r > 15 {
-			var avatarUrl = message.Member.AvatarURL("")
-			var username = message.Member.Nick
-			webhook.WriteMessage(session, message.ChannelID, response, username, avatarUrl)
-		}
+	if message.Author.ID == b.id() && roll20(15) {
+		var avatarUrl = message.Member.AvatarURL("")
+		var username = message.Member.Nick
+		webhook.WriteMessage(session, message.ChannelID, b.Response(), username, avatarUrl)
 	}
 }
