c6d1a9f614566b0526407b505fbfa365f70d557e
Andrew <covadax.ag@gmail.com>
2022-08-09 00:37:16 -0400

    implemented sheeshbot

---
+++ internal/replybot/SeeshBot.go
@@ -0,0 +1,41 @@
+package replybot
+
+import (
+	"fmt"
+	"golang-discord-bot/internal/config"
+	"golang-discord-bot/internal/webhook"
+	"golang-discord-bot/utils"
+	"math/rand"
+	"strings"
+
+	"github.com/bwmarrin/discordgo"
+)
+
+type SheeshBot struct {
+}
+
+func (b SheeshBot) id() string {
+	return config.UserIDs["Guy"]
+}
+
+func (b SheeshBot) ObserverName() string {
+	return "SheeshBot"
+}
+
+func (b SheeshBot) AvatarURL() string {
+	return "https://imgur.com/4CqBg7F.png"
+}
+
+func (b SheeshBot) Pattern() string {
+	return "she+sh"
+}
+
+func (b SheeshBot) Response() string {
+	return fmt.Sprintf("sh%ssh", strings.Repeat("e", rand.Intn(500)))
+}
+
+func (b SheeshBot) HandleMessage(session *discordgo.Session, message *discordgo.Message) {
+	if utils.Match(b.Pattern(), message.Content) || message.Author.ID == b.id() {
+		webhook.WriteMessage(session, message.ChannelID, b.Response(), b.ObserverName(), b.AvatarURL())
+	}
+}
