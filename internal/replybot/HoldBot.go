8d054d92d9dea74118247600fb8134ccfa046cc7
Andrew <covadax.ag@gmail.com>
2022-08-09 00:02:47 -0400

    Implemented HoldBot

---
+++ internal/replybot/HoldBot.go
@@ -0,0 +1,33 @@
+package replybot
+
+import (
+	"golang-discord-bot/internal/webhook"
+	"golang-discord-bot/utils"
+
+	"github.com/bwmarrin/discordgo"
+)
+
+type HoldBot struct {
+}
+
+func (b HoldBot) ObserverName() string {
+	return "HoldBot"
+}
+
+func (b HoldBot) AvatarURL() string {
+	return "https://i.imgur.com/YPFGEzM.png"
+}
+
+func (b HoldBot) Response() string {
+	return "Hold."
+}
+
+func (b HoldBot) Pattern() string {
+	return "^Hold\\.?$"
+}
+
+func (b HoldBot) HandleMessage(session *discordgo.Session, message *discordgo.Message) {
+	if utils.Match(b.Pattern(), message.Content) {
+		webhook.WriteMessage(session, message.ChannelID, b.Response(), b.ObserverName(), b.AvatarURL())
+	}
+}
